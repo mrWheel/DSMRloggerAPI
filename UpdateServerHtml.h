f537b35f0c5bfeba86ab88c2576c3f70a894eae4
mrWheel <5585427+mrWheel@users.noreply.github.com>
2020-01-06 17:33:49 +0100

    Update UpdateServerHtml.h

--- UpdateServerHtml.h
+++ UpdateServerHtml.h
@@ -6,17 +6,15 @@ static const char UpdateServerIndex[] PROGMEM =
      </style>
      <body>
      <h1>DSMR-logger Flash utility</h1>
-     <form method='POST' action='?cmd=0' enctype='multipart/form-data'>
+     <form method='POST' action='' enctype='multipart/form-data'>
         Selecteer een "<b>.ino.bin</b>" bestand<br/>
-        <input type='hidden' name='cmd' value='0'>
-                  <input type='file' accept='ino.bin' name='firmware'>
-                  <input type='submit' value='Flash Firmware'>
+        <input type='file' accept='ino.bin' name='firmware'>
+        <input type='submit' value='Flash Firmware'>
       </form>
-      <form method='POST' action='?cmd=100' enctype='multipart/form-data'> 
+      <form method='POST' action='' enctype='multipart/form-data'> 
         Selecteer een "<b>.spiffs.bin</b>" bestand<br/>
-        <input type='hidden' name='cmd' value='100'>
-                  <input type='file' accept='spiffs.bin' name='filesystem'>
-                  <input type='submit' value='Flash Spiffs'>
+        <input type='file' accept='spiffs.bin' name='filesystem'>
+        <input type='submit' value='Flash Spiffs'>
       </form>
       <br/>Wacht nog <span style='font-size: 1.3em;' id="waitSeconds">120</span> seconden ..
       <br>Als het lijkt of er niets gebeurd, wacht dan tot de teller
