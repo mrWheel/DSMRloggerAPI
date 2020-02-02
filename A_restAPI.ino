78325b41a6ecf81ad8c72632db17be67807c5950
Robert van den Breemen <robert@vandenbreemen.net>
2020-02-02 11:17:33 +0100

    typo corrected

--- A_restAPI.ino
+++ A_restAPI.ino
@@ -446,7 +446,7 @@ void sendJsonHist(int8_t fileType, const char *fileName, const char *timeStamp, 
 {
   uint8_t startSlot, nrSlots, recNr  = 0;
   char    typeApi[10];
-  DECLARE_TIME_SEC(antiWearTimer, 61);
+  DECLARE_TIMER_SEC(antiWearTimer, 61);
 
   if (DUE(antiWearTimer))
   {
