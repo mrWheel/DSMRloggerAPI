1b9a6b7dc5877ebb0afdfbfb2556b409ff7335ed
mrWheel <5585427+mrWheel@users.noreply.github.com>
2020-03-14 13:22:24 +0100

    v1.0.1 API doc as SPA

--- data/DSMRindex.js
+++ data/DSMRindex.js
@@ -27,6 +27,7 @@
   var gd_tariff           = 0;
   var electr_netw_costs   = 0;
   var gas_netw_costs      = 0;
+  var hostName            = "-";
   
   var data       = [];
   
@@ -107,10 +108,8 @@
                                                 {openTab('TelegramTab');});
     document.getElementById('bSysInfoTab').addEventListener('click',function() 
                                                 {openTab('SysInfoTab');});
-    document.getElementById('restAPITab').addEventListener('click',function() 
-                                                { console.log("newPage: goAPI");
-                                                  location.href = "/api";
-                                                });
+    document.getElementById('bAPIdocTab').addEventListener('click',function() 
+                                                {openTab('APIdocTab');});
     document.getElementById('FSexplorer').addEventListener('click',function() 
                                                 { console.log("newTab: goFSexplorer");
                                                   location.href = "/FSexplorer";
@@ -208,6 +207,11 @@
       console.log("newTab: TelegramTab");
       refreshSmTelegram();
       //tabTimer = setInterval(refreshSmTelegram, 60 * 1000); // do not repeat!
+
+    } else if (tabName == "APIdocTab") {
+      console.log("newTab: APIdocTab");
+      showAPIdoc();
+    
     }
 
   } // openTab()
@@ -930,6 +934,10 @@
             {
               gas_netw_costs = json.settings[i].value;
             }
+            else if (json.settings[i].name == "hostname")
+            {
+              hostName = json.settings[i].value;
+            }
           }
       })
       .catch(function(error) {
@@ -976,6 +984,8 @@
       presentationType = "TAB";
     }
 
+    document.getElementById("APIdocTab").style.display = "none";
+
     if (activeTab == "ActualTab")  refreshSmActual();
     if (activeTab == "HoursTab")   refreshHours();
     if (activeTab == "DaysTab")    refreshDays();
@@ -1006,6 +1016,76 @@
     refreshMonths();
       
   } // setMonthTableType()
+
+    
+  //============================================================================  
+  function showAPIdoc() {
+    console.log("Show API doc ..");
+    document.getElementById("APIdocTab").style.display = "block";
+    addAPIdoc("/api/v1/dev/info", "Device info in JSON format", true);
+    addAPIdoc("/api/v1/dev/time", "Device time (epoch) in JSON format", true);
+    addAPIdoc("/api/v1/dev/settings", "Device settings in JSON format", true);
+    addAPIdoc("/api/v1/dev/settings{jsonObj}", "[POST] update Device settings in JSON format\
+        <br>test with:\
+        <pre>curl -X POST -H \"Content-Type: application/json\" --data '{\"name\":\"mqtt_broker\",\"value\":\"hassio.local\"}' \
+http://DSMR-API.local/api/v1/dev/settings</pre>", false);
+    
+    addAPIdoc("/api/v1/sm/info", "Smart Meter info in JSON format", true);
+    addAPIdoc("/api/v1/sm/actual", "Smart Meter Actual data in JSON format", true);
+    addAPIdoc("/api/v1/sm/fields", "Smart Meter all fields data in JSON format\
+        <br>JSON format: {\"fields\":[{\"name\":\"&lt;fieldName&gt;\",\"value\":&lt;value&gt;,\"unit\":\"&lt;unit&gt;\"}]} ", true);
+    addAPIdoc("/api/v1/sm/fields/{fieldName}", "Smart Meter one field data in JSON format", false);
+
+    addAPIdoc("/api/v1/sm/telegram", "raw telegram as send by the Smart Meter including all \"\\r\\n\" line endings", false);
+
+    addAPIdoc("/api/v1/hist/hours", "History data per hour in JSON format", true);
+    addAPIdoc("/api/v1/hist/days", "History data per day in JSON format", true);
+    addAPIdoc("/api/v1/hist/months", "History data per month in JSON format", true);
+
+  } // showAPIdoc()
+
+    
+  //============================================================================  
+  function addAPIdoc(restAPI, description, linkURL) {
+    if (document.getElementById(restAPI) == null)
+    {
+      var topDiv = document.getElementById("APIdocTab");
+      var br = document.createElement("BR"); 
+      br.setAttribute("id", restAPI, 0);
+      br.setAttribute("style", "clear: left;");
+      
+      // <div class='div1'><a href="http://dsmr-api.local/api/v1/dev/time" target="_blank">/api/v1/dev/time</a></div>
+      var div1 = document.createElement("DIV"); 
+      div1.setAttribute("class", "div1", 0);
+      var aTag = document.createElement('a');
+      if (linkURL)
+      {
+        aTag.setAttribute('href',"http://" +hostName +".local" +restAPI);
+        aTag.target = '_blank';
+      }
+      else
+      {
+        aTag.setAttribute('href',"#");
+      }
+      aTag.innerText = restAPI;
+      aTag.style.fontWeight = 'bold';
+      div1.appendChild(aTag);
+
+      // <div class='div2'>Device time (epoch) in JSON format</div>
+      var div2 = document.createElement("DIV"); 
+      div2.setAttribute("class", "div2", 0);
+      //var t2 = document.createTextNode(description);                   // Create a text node
+      var t2 = document.createElement("p");
+      t2.innerHTML = description;                   // Create a text node
+      div2.appendChild(t2);     
+
+      topDiv.appendChild(br);    // Append <br> to <div> with id="myDIV"
+      topDiv.appendChild(div1);  // Append <div1> to <topDiv> with id="myDIV"
+      topDiv.appendChild(div2);  // Append <div2> to <topDiviv> with id="myDIV"
+    }
+    
+    
+  } // addAPIdoc()
   
   
   //============================================================================  
